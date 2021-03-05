22c945c76737dddb524f9f94cf181353d8d62456
Adrian <adrianlandag@gmail.com>
2021-03-04 17:57:03 -0800

    4.3 Lesson completed

---
+++ Documents/ucla/sandbox/tutor/review-js/assets/js/script.js
@@ -0,0 +1,16 @@
+pseudocode
+//declare variable called age that will store a number
+Var age = 18 
+//check if age greater than 18 and if so log out "you can vote"
+if (age === '18') {
+    console.log ("you can vote");
+}
+//esle if age greater than 21 or greater "you can drink"
+else if (age < 21) {
+    console.log ("you can drink");
+}
+
+//otherwise "you can not do anything"
+else {
+    console.log ('you can not do anything')
+}
